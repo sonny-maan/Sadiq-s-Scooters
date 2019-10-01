627d5c00c41a4c73fa04379f316af89f2e2b8fa6
dtrts <djt210@gmail.com>
2019-10-01 10:58:36 +0100

    People can walk at angles and have custom stats.
    Co-authored-by: jodifoster <51475537+JodiFoster@users.noreply.github.com>
    Co-authored-by: fardenti <38498908+fardenti@users.noreply.github.com>

--- test/person.test.js
+++ test/person.test.js
@@ -15,6 +15,7 @@ describe('person', () => {
   });
 
   test('returns true if person is at destination', () => {
+    console.log(person)
     for (var i = 0; i < 51; i++) {
       person.walk();
     }
@@ -27,14 +28,36 @@ describe('custom person', () => {
 
   test('person moves to destination if within speed', () => {
     person = new Person({
-      location: [0.05, 0.1],
-      destination: [0.05, 0.8],
+      location: [0.1, 0.1],
+      destination: [0.8, 0.8],
       speed: 1
     })
     expect(person.atDestination()).toEqual(true);
     person.walk()
     expect(person.atDestination()).toEqual(true);
-    expect(person.location).toEqual([0.05, 0.8])
+    expect(person.location).toEqual([0.8, 0.8])
 
   });
+
+
+  test('person moves to destination at angles', () => {
+    person = new Person({
+      location: [0.8, 0.9],
+      destination: [0.2, 0.38],
+      speed: 0.02
+    })
+    expect(person.atDestination()).toEqual(false);
+    steps = 0
+    while (!person.atDestination() && steps < 1000) {
+      steps++
+      person.walk()
+    }
+
+    expect(person.atDestination()).toEqual(true);
+    person.walk();
+    expect(person.location).toEqual([0.2, 0.38])
+
+  });
+
+
 });
