58ee060ad3e1b35eb893980ac692e5da0fd2528e
dtrts <djt210@gmail.com>
2019-10-02 11:23:23 +0100

    World generates docking stations
    Co-authored-by: sonny-maan <42817066+sonny-maan@users.noreply.github.com>
    Co-authored-by: jaitone <51136692+jaitone@users.noreply.github.com>

--- test/feature.test.js
+++ test/feature.test.js
@@ -45,23 +45,39 @@ describe('world', () => {
     expect(world.people.length).toEqual(0); // still at 0.5?
   });
 
-test('person will use a scooter when it goes past a docking station, and will put it back at the end' , () => {
-  let dockingStation1 = world.generateDockingStation({location: [0,0.6]})
-  let dockingStation2 = world.generateDockingStation({location: [0,0.8]})
-  expect(world.dockingStation.length).toEqual(2);
-  let person = world.generatePerson();
-  let stepCounter = 0
-  while ((person.location != dockingStation1.location) && (stepCounter < 2000))
-  {
-    world.tick();
-    stepCounter++
-  }
-  expect(person.vehicle).toEqual(true)
-  while ((person.location != dockingStation2.location) && (scootCounter < 2000))
-  {world.tick();
-  scootCounter++ }
-  expect(scootCounter).toBeLessThan(8)
-  expect(person.vehicle).toEqual(false)
+  test('person will use a scooter when it goes past a docking station, and will put it back at the end', () => {
+    let dockingStation1 = world.generateDockingStation({
+      location: [0, 0.6]
+    })
+    let dockingStation2 = world.generateDockingStation({
+      location: [0, 0.8]
+    })
+    expect(world.dockingStations.length).toEqual(2);
 
- });
+    let person = world.generatePerson({
+      location: [0, 0],
+      destination: [0, 1]
+    });
+    let stepCounter = 0
+
+    while ((person.location != dockingStation1.location) && (stepCounter < 2000)) {
+      world.tick();
+      stepCounter++
+    }
+
+    expect(person.vehicle).toEqual(true)
+    while ((person.location != dockingStation2.location) && (scootCounter < 2000)) {
+      world.tick();
+      scootCounter++
+    }
+
+    expect(scootCounter).toBeLessThan(8)
+
+    expect(person.vehicle).toEqual(false)
+
+  });
+
+
+
+
 });
