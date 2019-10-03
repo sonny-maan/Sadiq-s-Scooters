ebbd9306484cc5794ada33f251b58de23938053f
Student <farnazardenti@gmail.com>
2019-10-03 22:32:15 +0100

    Added start button, stylesheets for button, resized canvas

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 // const Person = require('../lib/person');
 // const World = require('../lib/world');
-
-
-
-var body = document.getElementsByTagName("body")[0];
-var canvas = document.createElement("canvas");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-var context = canvas.getContext("2d");
-body.appendChild(canvas);
+function start(){
+  document.getElementById("start-btn").style.display = 'none';
+  canvas.width = 700;
+  canvas.height = 700;
+  document.body.insertBefore(canvas, document.body.childNodes[0]);
+}
 
 world = new World
-
-console.log('new world!')
-console.log(world)
-
+person = new Person
+// console.log(person === )
 function create() {
   //  Opacity
   // world.generatePerson()
@@ -28,73 +23,37 @@ function create() {
     path.push([Math.random(), Math.random()])
   }
   let options = {
-    location: [Math.random(), Math.random()],
-    destination: [Math.random(), Math.random()],
-    path: [],
-    // path: path,
+    location: [0, 0],
+    destination: [0, 0],
+    path: path,
     speed: Math.random() / 10
   }
   world.generatePerson(options)
-
-
   console.log(world.people);
-
 }
 
 function drawPeople() {
-  const context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
   world.tick();
   // Remove people
   // Redraw all people
   var width = 20;
   var height = 20;
-
-  world.people.forEach(person1 => {
-    if (person1.onVehicle) {
-      context.fillRect(person1.location[0] * canvas.width, person1.location[1] * canvas.height, width + 20, height);
-    } else {
-      context.fillRect(person1.location[0] * canvas.width, person1.location[1] * canvas.height, width, height);
-    }
-    context.lineWidth = 1
-    context.strokeStyle = 'red'
-    context.beginPath();
-    context.moveTo(person1.location[0] * canvas.width, person1.location[1] * canvas.height);
-    context.lineTo(person1._destination[0] * canvas.width, person1._destination[1] * canvas.height);
-    context.stroke();
-
-    if (person1._path[0]) {
-      context.strokeStyle = 'green'
-      context.beginPath();
-
-      context.moveTo(person1.location[0] * canvas.width, person1.location[1] * canvas.height);
-      context.lineTo(person1._path[0][0] * canvas.width, person1._path[0][1] * canvas.height);
-      context.stroke();
-      context.strokeStyle = 'blue'
-      context.beginPath();
-      context.moveTo(person1._destination[0] * canvas.width, person1._destination[1] * canvas.height);
-      context.lineTo(person1._path[0][0] * canvas.width, person1._path[0][1] * canvas.height);
-      context.stroke();
-    }
-
+  people = world.people
+  people.forEach(Person => {
+    context.fillRect(Person._location[0] * canvas.width, Person._location[1] * canvas.height, width, height);
   });
-
-  world.dockingStations.forEach(dockingStation => {
-    let eh1 = dockingStation._location[0]
-    let eh2 = dockingStation._location[1]
-    context.fillRect(eh1 * canvas.width, eh2 * canvas.height, 5, 15);
-  });
-
-
   setTimeout(drawPeople, 50)
 }
 // setInterval(drawPeople, 50)
-drawPeople()
 
 // function Square(width, height, color, x, y) {
 //     this.Person = world.generatePerson()
-
 //     context.fillRect(person._location[0], person._location[1], width, height);
 //   }
 
-document.addEventListener('click', create);
+function startGame() {
+  start();
+  create();
+  drawPeople()
+}
